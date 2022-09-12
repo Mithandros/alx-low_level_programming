e1be19672a8ae26bf49c2e6eae2f04955e6ecb61
Precious Ihire <preciousihire@gmail.com>
2022-09-11 21:04:03 -0700

    Made Task 4 Betty compliant

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,7 +7,10 @@
 * Return: 0
 */
 int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle");
-       	return (0);
+{	
+	puts("\"Programming is like building a multilingual puzzle");	
+	return (0);
+
 }
+	
+       	
