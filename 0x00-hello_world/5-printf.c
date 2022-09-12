0bfbe9436a9b4aab179d8d0327237fe89d4a0df5
Precious Ihire <preciousihire@gmail.com>
2022-09-12 15:09:02 -0700

    Task 5 corrected

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
-* main - print the string in the put function
+* main - print the string in the printf function
 *
-* this program prints "Programming is like building a multilingual puzzle
+* this program prints "with proper grammar, but the outcome is a piece of art
 * Return:0
 */
 int main(void)
