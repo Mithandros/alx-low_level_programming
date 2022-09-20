1ff6fb453dbd4e2870f426666d619a9bf43aa34b
Precious Ihire <preciousihire@gmail.com>
2022-09-20 23:15:45 +0100

    Update 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
-/***
+/**
  * _strlen - Returns the length of a string
+ 
  * @s: String to count
  *
  * Return: String length
@@ -9,6 +10,7 @@
 int _strlen(char *s)
 {
 	int c = 0;
+	
 	for (; *s != '\0'; s++)
 	{
 		c++;
