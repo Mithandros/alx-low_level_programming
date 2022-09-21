aea311926de4279aa0f03d64fea02f4d92bedcd5
Precious Ihire <preciousihire@gmail.com>
2022-09-21 04:07:49 +0100

    Update 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -6,11 +6,11 @@
  * Return: returns length as integer;
  */
 
-int _strlen (char *s)
+int _strlen(char *s)
 {
 	int len = 0;
 
-	while (*(s+len) != '\0')
+	while (*(s + len) != '\0')
 		len++;
 
 	return (len);
